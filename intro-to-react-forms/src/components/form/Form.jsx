58ee66d6f14256dd7dcb701e9eb3def9fea8e5c8
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-31 17:01:39 -0700

    add red error fields

--- intro-to-react-forms/src/components/form/Form.jsx
+++ intro-to-react-forms/src/components/form/Form.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import { useState } from "react";
 import './Form.css';
 
 const Form = props => {
@@ -59,10 +59,12 @@ const Form = props => {
 
     const handleSubmit = e => {
         e.preventDefault();
-        const resultErrors = validate();
+        const resultErrors = validate();        
         
-        if (errors.length === 0) {
-            console.log(user);
+        if (Object.keys(resultErrors).length === 0) {
+            const updatedUser = {...user, submissionTime: new Date().toISOString()};
+
+            console.log(updatedUser);
 
             setUser({
                 name: '',
@@ -79,28 +81,31 @@ const Form = props => {
     return (
         <form onSubmit={handleSubmit}>
             <label>Name:
-                <input type="text" value={user.name} onChange={handleChange('name')} />
+                <input type="text" value={user.name} onChange={handleChange('name')} className={errors.name ? 'input-error' : ''} />
                 {errors.name && <p className="error">{errors.name}</p>}
             </label>
+            <br></br>
 
             <label>Email:
-                <input type="text" value={user.email} onChange={handleChange('email')} />
+                <input type="text" value={user.email} onChange={handleChange('email')} className={errors.email ? 'input-error' : ''} />
                 {errors.email && <p className="error">{errors.email}</p>}
             </label>
+            <br></br>
 
             <label>Phone Number:
-                <input type="text" value={user.phoneNumber} onChange={handleChange('phoneNumber')} />
+                <input placeholder="XXX-XXX-XXXX" type="text" value={user.phoneNumber} onChange={handleChange('phoneNumber')} className={errors.phoneNumber ? 'input-error' : ''} />
                 {errors.phoneNumber && <p className="error">{errors.phoneNumber}</p>}
             </label>
+            <br></br>
             
-            <select value={user.phoneType} onChange={handleChange('phoneType')}>
+            <select value={user.phoneType} disabled={user.phoneNumber.length < 12} onChange={handleChange('phoneType')} className={errors.phoneType ? 'input-error' : ''}>
                 <option value="" disabled>Select a phone type:</option>
                 <option value="home">Home</option>
                 <option value="work">Work</option>
                 <option value="mobile">Mobile</option>
             </select>
             {errors.phoneType && <p className="error">{errors.phoneType}</p>}
-
+            <br></br>
 
             <p>Staff:</p>
             <label>Instructor
@@ -110,13 +115,16 @@ const Form = props => {
             <label>Student
                 <input type="radio" name="staff" value="student" onChange={handleChange('staff')} />
             </label>
+            <br></br>
 
-            <textarea placeholder="Tell us about yourself" value={user.bio} onChange={handleChange('bio')}></textarea>
+            <textarea placeholder="Tell us about yourself" value={user.bio} onChange={handleChange('bio')} className={errors.bio ? 'input-error' : ''}></textarea>
             {errors.bio && <p className="error">{errors.bio}</p>}
+            <br></br>
 
             <label>Do you want to subscribe to email notifications?
                 <input type="checkbox" value="Yes" onChange={({target}) => setUser(prev => ({...prev, emailNotifications: target.checked ? 'Yes' : 'No'}))}/>
             </label>
+            <br></br>
 
             <button>Create User</button>
         </form>
